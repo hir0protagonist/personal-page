17c0a830efe4a11ed8189259397fa80584072a41
Sergey Poteryuha <s.poteryuha@gmail.com>
2026-01-15 23:52:31 +0600

    Remove suspense boundary for posts list

--- features/blog/components/PostsList/PostsList.tsx
+++ features/blog/components/PostsList/PostsList.tsx
@@ -1,17 +1,13 @@
 import Header from '@/ui/components/Header/Header';
 import styles from './PostsList.module.css';
 import PostsListItems from '../PostsListItems/PostsListItems';
-import { Suspense } from 'react';
-import Loader from '@/ui/components/Loader/Loader';
 
 export default async function PostList() {
     return (
         <div>
             <Header title='[Not a] Blog' />
             <section className={styles.posts}>
-                <Suspense fallback={<Loader />}>
-                    <PostsListItems></PostsListItems>
-                </Suspense>
+                <PostsListItems></PostsListItems>
             </section>
         </div>
     );
